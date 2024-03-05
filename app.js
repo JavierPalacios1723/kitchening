c70b6b75b4f55824cfdb3005ae18ef2ad8f8e2db
JavierPalacios1723 <javierpalacios257@gmail.com>
2024-03-05 18:45:33 -0300

    cambios en archivos

--- app.js
+++ app.js
@@ -3,28 +3,35 @@ const app = express()
 const path = require('path')
 const port = 3030;
 
+/* RUTAS */
+const authRoutes = require('./routes/authentication.routes')
+const cartRoutes = require('./routes/cart.routes')
+const otherRoutes = require("./routes/other.routes")
 
+/* CONFIGURACIONES */
 app.use(express.static('public'))
 
-app.get('/',(req,res) => {
-  res.sendFile(path.join(__dirname,'./views/home.html'))
-})
 
-app.get('/home',(req,res) => {
-  res.redirect('/');
-})
+/* ENRUTADORES */
+app.use("/", otherRoutes)
+app.use("/autenticacion", authRoutes)
+app.use("/carrito", cartRoutes)
 
-app.get('/registro',(req, res) => {
-  res.sendFile(path.join(__dirname,'./views/register.html'))
-})
+// app.get('/home',(req,res) => {
+//   res.redirect('/');
+// })
 
-app.get('/pseudoclases',(req, res) => {
-  res.sendFile(path.join(__dirname,'./views/pseudoclases.html'))
-})
+// app.get('/registro',(req, res) => {
+//   res.sendFile(path.join(__dirname,'./views/register.html'))
+// })
 
-app.get('/position',(req, res) => {
-  res.sendFile(path.join(__dirname,'./views/position.html'))
-})
+// app.get('/pseudoclases',(req, res) => {
+//   res.sendFile(path.join(__dirname,'./views/pseudoclases.html'))
+// })
+
+// app.get('/position',(req, res) => {
+//   res.sendFile(path.join(__dirname,'./views/position.html'))
+// })
 
 app.listen(port,() => console.log(`http://localhost:${port}`))
 
